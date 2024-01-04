868edce1dc474c525fafb72203e759b8ab46c950
Tom-Brd <bourlard.tom.94@gmail.com>
2024-01-04 21:00:50 +0100

    feat(jsonReader): method to open JsonFile

---
+++ src/main/java/com/tom/secretSanta/JsonReader.java
@@ -0,0 +1,21 @@
+package com.tom.secretSanta;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class JsonReader {
+    private JSONArray openJsonFile(String filename) throws IllegalAccessException {
+        try (InputStream inputStream = JsonReader.class.getClassLoader().getResourceAsStream(filename)) {
+            if (inputStream == null) {
+                throw new IllegalAccessException("Could not find file " + filename);
+            }
+            JSONParser parser = new JSONParser();
+            return (JSONArray) parser.parse(new InputStreamReader(inputStream));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error reading file " + filename + ": " + e.getMessage());
+        }
+    }
+}
